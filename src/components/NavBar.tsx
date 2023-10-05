e546db2f179a2f09f5f5dfb0e4e6a5be409759c4
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2023-10-06 00:16:36 +0300

    header mobile menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import logo from '@/assets/main-page/logo.png'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import { MapPinIcon } from '@heroicons/react/24/outline'
 
 type Props = {}
 
@@ -7,16 +9,24 @@ const NavBar = (props: Props) => {
   const [menuIsToggled, setMenuIsToggled] = useState(false)
   return (
     <nav>
-      <div>
-        <div>
-          <a href="#">
-            <img src={logo} alt="" />
-          </a>
-          <div>
-            <span></span>
+      <div className="bg-white text-[#666]">
+        <div className="h-[70px] px-6 border-b border-[#BBBBBB] flex  justify-between">
+          <div className="flex items-center">
+            <a href="#" className="rounded-full">
+              <img className="w-9 h-9 rounded-full " src={logo} alt="logo" />
+            </a>
           </div>
-          <div>
-            <img src="" alt="" />
+
+          <div className="flex items-center pr-4">
+            <button className="relative pl-10 pr-4 h-full transition hover:before:content-[''] hover:before:bottom-0 hover:before:left-0 hover:before:absolute hover:before:w-full hover:before:h-[3px] hover:before:bg-[#1C69D4] flex flex-col justify-center">
+              <MapPinIcon className="absolute w-6 h-6 left-4 top-[50%] translate-y-[-50%]" />
+              <span className="text-[0.875rem] w-full font-bold leading-5 ml-1">
+                Choose your local BMW Center
+              </span>
+            </button>
+            <button className="w-6 h-6 ml-3">
+              <XMarkIcon className="w-6 h-6 hover:text-[#191f3f]" />
+            </button>
           </div>
         </div>
         <div>
