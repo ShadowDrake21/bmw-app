76345877053de8268be0374cd9122a77904313f3
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2023-10-12 23:01:13 +0300

    best models block + items are done

--- src/components/BestModels/BestModels.tsx
+++ src/components/BestModels/BestModels.tsx
@@ -6,8 +6,8 @@ type Props = {}
 
 const BestModels = (props: Props) => {
   return (
-    <div>
-      <div>
+    <div className="pt-3 sm:mx-[4%] sm:pt-9">
+      <div className="flex flex-wrap gap-1 sm:grid sm:grid-cols-[auto_auto] sm:gap-10 xl:gap-11">
         {bestModelsInfo.map((bestModel, index) => (
           <BestModelsItem {...bestModel} key={index} />
         ))}
--- src/components/BestModels/BestModelsItem.tsx
+++ src/components/BestModels/BestModelsItem.tsx
@@ -9,8 +9,23 @@ const BestModelsItem = (model: IBestModels) => {
   const { images, name, description, link } = model
   const { desktop, tablet, mobile } = images
   return (
-    <div>
+    <div className="relative w-full">
+      <div className="absolute bottom-[40px] left-0 right-0 text-center z-30 sm:bottom-4">
+        <h5 className="text-[11px] font-bold leading-3 tracking-[2.75px] text-white mb-[2px] xl:text-xs">
+          {name}
+        </h5>
+        <h3 className="text-white text-xl tracking-[-0.1px] font-normal mb-1 sm:text-[23px] sm:leading-[31px] xl:text-[28px] xl:leading-[37px] sm:mb-2">
+          {description}
+        </h3>
+        <a
+          href={link}
+          className="text-[#cacaca] text-sm font-light transition-all duration-150 hover:text-white underline"
+        >
+          Learn more
+        </a>
+      </div>
       <img
+        className="object-cover w-full"
         src={width < 768 ? mobile : width >= 1280 ? desktop : tablet}
         alt="best-model-img"
       />
