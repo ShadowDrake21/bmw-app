32cdd391be2afb20c628943cf215678b83f3f2c9
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2023-10-22 00:19:08 +0300

    VehicleFirstScreen (responsive) is done

--- src/components/VehicleAddNavBar/VehicleAddNavBar.tsx
+++ src/components/VehicleAddNavBar/VehicleAddNavBar.tsx
@@ -1,32 +1,90 @@
 import { ChevronDownIcon } from '@heroicons/react/24/solid'
+import { useWindowDimensions } from '../../utils/utils'
+import { useState } from 'react'
+
+const xlLinks = [
+  { title: 'overview' },
+  { title: 'models & features' },
+  { title: 'build your own' },
+]
 
 const VehicleAddNavBar = ({ isTopOfPage, title, link }: any) => {
+  const [xlLinkClick, setXlLinkClick] = useState<Number>(0)
+
+  const { width } = useWindowDimensions()
+  const widthCheckXL = () => {
+    return width < 1280
+  }
   return (
     <div
       className={`${
         isTopOfPage
-          ? 'absolute border-t border-[hsla(0,0%,79.2%,.7)] top-[70px]'
+          ? 'absolute border-t border-[hsla(0,0%,79.2%,.7)] top-[70px] xl:border-none xl:top-[84px]'
           : 'fixed top-70px bg-[#f6f6f6] border-[#cacaca] border-b'
-      } w-full transition-[top_0.4s_ease_0s] z-50`}
+      } w-full transition-all z-[18]`}
     >
       <div className="py-[14px] px-[4%]">
-        <div className="flex justify-between">
+        <div className="flex justify-between xl:items-center">
           <div className="font-bold cursor-pointer">
             <span
-              className={`${isTopOfPage ? 'text-[#f6f6f6]' : 'text-[#221f1f]'}`}
+              className={`${
+                isTopOfPage ? 'text-[#f6f6f6]' : 'text-[#221f1f]'
+              } text-sm tracking-[0.25px] sm:text-[15px] sm:leading-[18px]`}
             >
               {title}
             </span>
-            <div
-              className={`${
-                isTopOfPage ? 'text-[#cacaca]' : 'text-[#4c4c4c]'
-              } relative pt-[5px] text-xs tracking-[.25px] pr-4`}
-            >
-              <span>Overview</span>
-              <ChevronDownIcon className="absolute w-[10px] right-0 top-[10px]" />
-            </div>
+            {widthCheckXL() && (
+              <div
+                className={`${
+                  isTopOfPage ? 'text-[#cacaca]' : 'text-[#4c4c4c]'
+                } relative pt-[5px] text-xs tracking-[.25px] pr-4 sm:text-[13px] sm:leading-4`}
+              >
+                <span>Overview</span>
+                <ChevronDownIcon className="absolute w-[10px] right-0 top-[10px]" />
+              </div>
+            )}
           </div>
-
+          <div className="hidden xl:block">
+            <ul className="flex gap-10">
+              {xlLinks.map((xlLink, index) => (
+                <li
+                  key={index}
+                  className={`${
+                    xlLinkClick === index && isTopOfPage
+                      ? 'text-[#f6f6f6] relative pb-2 after:absolute after:w-full after:h-[3px] after:content-[""] after:bg-[#f6f6f6] after:bottom-0 after:left-0'
+                      : `${
+                          xlLinkClick === index && !isTopOfPage
+                            ? 'text-[#1c69d3] relative pb-2 after:absolute after:w-full after:h-[3px] after:content-[""] after:bg-[#1c69d3] after:bottom-0 after:left-0'
+                            : `${
+                                xlLinkClick !== index && isTopOfPage
+                                  ? 'text-[#cacaca] hover:text-white'
+                                  : `${
+                                      xlLinkClick !== index &&
+                                      !isTopOfPage &&
+                                      'text-[#221f1f] hover:text-[#1c69d3]'
+                                    } `
+                              } `
+                        } `
+                  } transition-colors duration-300`}
+                  onClick={() => setXlLinkClick(index)}
+                >
+                  <a
+                    href={`${index !== 1 ? '#' : link}`}
+                    className={`${xlLinkClick === index && 'pb-3'} ${
+                      index === 1 && 'pr-4'
+                    } relative h-full`}
+                  >
+                    <span className="text-[13px] leading-4 tracking-[.25px] capitalize">
+                      {xlLink.title}
+                    </span>
+                    {index === 1 && (
+                      <ChevronDownIcon className="absolute w-[10px] right-0 top-[7px]" />
+                    )}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className={`${isTopOfPage ? 'mr-4' : 'mr-0'}`}>
             <a
               href={link}
--- src/components/VehicleFirstScreen/VehicleFirstScreen.tsx
+++ src/components/VehicleFirstScreen/VehicleFirstScreen.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { IFirstScreenVideo } from '../../static/videoItems'
-import { useWindowDimensions } from '../../utils/utils'
 import leaseOfferIcon from '@/assets/model-i5/firstScreen/lease_offer-svg.svg'
 import VehicleAddNavBar from '../VehicleAddNavBar/VehicleAddNavBar'
 
@@ -17,10 +16,6 @@ const VehicleFirstScreen = (firstScreenInfo: IFirstScreenVideo) => {
     laserOfferDescription,
     importantInfoLink,
   } = firstScreenInfo
-  const { width } = useWindowDimensions()
-  const widthCheckXL = () => {
-    return width < 1280
-  }
 
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
 
@@ -43,7 +38,7 @@ const VehicleFirstScreen = (firstScreenInfo: IFirstScreenVideo) => {
         link={btnLink}
       />
       <div className="max-h-screen h-full">
-        <div>
+        <div className="relative">
           <video
             src={videoURL}
             autoPlay
@@ -51,26 +46,36 @@ const VehicleFirstScreen = (firstScreenInfo: IFirstScreenVideo) => {
             muted
             className="min-h-[500px] object-cover w-full"
           />
-        </div>
-        <div className="first__screen-shadow relative bg-black text-white pb-[25px]">
-          <div className="mx-[4%] pt-1 pb-[15px]">
-            <h2 className="text-[32px] leading-[38px] font-normal relative z-20">
-              {mainTitle}
-            </h2>
-            <h1 className="text-xl tracking-[-.1px] font-normal mt-1 mb-5">
-              {mainDescription}
-            </h1>
-            <div className="mb-7">
+          <div className="hidden absolute w-full h-full top-0 text-white mx-[4%] sm:block">
+            <div className="absolute top-1/2 -translate-y-1/2 sm:w-[30%] xl:top-[38%] xl:flex xl:flex-col">
+              <h2 className="text-[32px] leading-[38px] font-normal relative z-20 top-0 sm:text-[42px] sm:leading-[50px] xl:text-[56px] xl:leading-[68px]">
+                {mainTitle}
+              </h2>
+              <h1 className="text-xl tracking-[-.1px] font-normal mt-1 mb-5 sm:text-[23px] sm:leading-[31px] xl:text-[28px] xl:leading-[37px] xl:mb-3">
+                {mainDescription}
+              </h1>
+              <div className="hidden mt-3 xl:block">
+                <a
+                  href={btnLink}
+                  className="inline-block rounded-sm px-7 py-3 bg-[#1c69d3] transition-all duration-100 hover:text-[#ebebeb] hover:bg-[#1d55a2] xl:pt-[18px] xl:pb-[17px] xl:px-[35]"
+                >
+                  <span className="text-[13px] leading-4 tracking-[.5px] font-bold xl:text-[15px] xl:leading-5">
+                    {btnText}
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="absolute bottom-12 xl:hidden">
               <a
                 href={btnLink}
-                className="inline-block rounded-sm px-7 py-3 bg-[#1c69d3] transition-all duration-100 hover:text-[#ebebeb] hover:bg-[#1d55a2]"
+                className="inline-block rounded-sm px-7 py-3 bg-[#1c69d3] transition-all duration-100 hover:text-[#ebebeb] hover:bg-[#1d55a2] xl:pt-[18px] xl:pb-[17px] xl:px-[35]"
               >
-                <span className="text-[13px] leading-4 tracking-[.5px] font-bold">
+                <span className="text-[13px] leading-4 tracking-[.5px] font-bold xl:text-[15px] xl:leading-5">
                   {btnText}
                 </span>
               </a>
             </div>
-            <div className="pb-12">
+            <div className="absolute right-[8%] bottom-5">
               <div className="pb-3">
                 <img src={icon} alt="icon" className="w-10" />
               </div>
@@ -79,7 +84,68 @@ const VehicleFirstScreen = (firstScreenInfo: IFirstScreenVideo) => {
                 offer details.
               </span>
             </div>
-            <div>
+          </div>
+          <div className="hidden text-[#f6f6f6] w-max absolute z-10 xl:block xl:bottom-[50px] xl:mx-[4%]">
+            <p className="relative pr-[17px] w-max mb-[10px]">
+              <span className="text-[10px] leading-[13px] font-bold tracking-[2px] uppercase">
+                Lease offer
+              </span>
+              <img
+                src={leaseOfferIcon}
+                alt="icon"
+                className="absolute w-3 h-3 right-0 top-2/3 -translate-y-1/2"
+              />
+            </p>
+            <p>
+              <span className="text-2xl font-normal tracking-[-.1px] sm:text-[30px] sm:leading-9 xl:text-[39px] xl:leading-[48px]">
+                {laserOfferPrice}
+              </span>
+              <span className="text-xs font-normal tracking-[.25px] sm:text-[13px] sm:leading-4">
+                /month
+              </span>
+            </p>
+            <p className="mt-[5px] text-xs tracking-[.25px]">
+              {laserOfferDescription}
+            </p>
+            <a
+              href={importantInfoLink}
+              className="inline-block text-[11px] leading-[13px] capitalize tracking-[.25px] border-b border-white sm:pt-[14px]"
+            >
+              important info
+            </a>
+          </div>
+        </div>
+        <div className="first__screen-shadow relative bg-black text-white pb-[25px] xl:hidden">
+          <div className="mx-[4%] pt-1 pb-[15px]">
+            <div className="block sm:hidden">
+              <h2 className="text-[32px] leading-[38px] font-normal relative z-20">
+                {mainTitle}
+              </h2>
+              <h1 className="text-xl tracking-[-.1px] font-normal mt-1 mb-5">
+                {mainDescription}
+              </h1>
+              <div className="mb-7">
+                <a
+                  href={btnLink}
+                  className="inline-block rounded-sm px-7 py-3 bg-[#1c69d3] transition-all duration-100 hover:text-[#ebebeb] hover:bg-[#1d55a2]"
+                >
+                  <span className="text-[13px] leading-4 tracking-[.5px] font-bold">
+                    {btnText}
+                  </span>
+                </a>
+              </div>
+              <div className="pb-12">
+                <div className="pb-3">
+                  <img src={icon} alt="icon" className="w-10" />
+                </div>
+                <span className="text-[10px] tracking-[.7px] font-normal">
+                  {secondaryDescription} Visit your authorized BMW Center for
+                  offer details.
+                </span>
+              </div>
+            </div>
+
+            <div className="sm:w-full sm:pt-10 sm:relative sm:after:absolute after:content-[''] after:h-full after:w-[1px] after:bg-[#4c4c4c] after:right-1/2 after:top-[15px] after:z-10">
               <div className="text-[#f6f6f6] w-max">
                 <p className="relative pr-[17px] w-max mb-[10px]">
                   <span className="text-[10px] leading-[13px] font-bold tracking-[2px] uppercase">
@@ -92,10 +158,10 @@ const VehicleFirstScreen = (firstScreenInfo: IFirstScreenVideo) => {
                   />
                 </p>
                 <p>
-                  <span className="text-2xl font-normal tracking-[-.1px]">
+                  <span className="text-2xl font-normal tracking-[-.1px] sm:text-[30px] sm:leading-9">
                     {laserOfferPrice}
                   </span>
-                  <span className="text-xs font-normal tracking-[.25px]">
+                  <span className="text-xs font-normal tracking-[.25px] sm:text-[13px] sm:leading-4">
                     /month
                   </span>
                 </p>
@@ -104,7 +170,7 @@ const VehicleFirstScreen = (firstScreenInfo: IFirstScreenVideo) => {
                 </p>
                 <a
                   href={importantInfoLink}
-                  className="text-[11px] leading-[13px] capitalize tracking-[.25px] border-b border-white"
+                  className="inline-block text-[11px] leading-[13px] capitalize tracking-[.25px] border-b border-white sm:pt-[14px]"
                 >
                   important info
                 </a>
